65de0ab5ba93ea9fe5292113d09a7a58552edea2
Soluciones Económicas y Analíticas (SEYA) <63426975+seya-ecuador@users.noreply.github.com>
2021-09-17 20:46:06 -0500

    Tip027

---
+++ Tips/Tip027/SEYA_pacman.R
@@ -0,0 +1,33 @@
+############################################################################
+############################################################################
+###                                                                      ###
+###                  SOLUCIONES ECONÓMICAS Y ANALÍTICAS                  ###
+###                              @SEYA_Ecu                               ###
+###                 ELABORACIÓN: @dhecheverria | @byfcl3                 ###
+###                            TIPS & TRICKS R                           ###
+###                                                                      ###
+############################################################################
+############################################################################
+
+
+library(pacman) #install.packages("pacman")
+
+# Ejemplo -----------------------------------------------------------------
+
+# Codificación con base
+
+library(dplyr)
+library(ggplot2)
+library(ggrepel)
+library(gganimate)
+library(ggdark)
+library(ggpubr)
+
+# Codificación con pacman
+
+p_load(dplyr,
+       ggplot2,
+       ggrepel,
+       gganimate,
+       ggdark,
+       ggpubr)
